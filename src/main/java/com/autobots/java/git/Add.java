09b92decbfbd2d9fbd05b4e183206b264ae542de
Ulugbek <attokurov51@mail.ru>
2025-07-02 12:03:06 +0600

    Git classes

---
+++ src/main/java/com/autobots/java/git/Add.java
@@ -0,0 +1,7 @@
+package com.autobots.java.git;
+
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/com/autobots/java/git/Log.java
@@ -0,0 +1,4 @@
+package com.autobots.java.git;
+
+public class Log {
+}
