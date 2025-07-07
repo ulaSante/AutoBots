7da247a6ee0e9a432ac84c7726c0be09b89b6e67
Ulugbek <attokurov51@mail.ru>
2025-07-07 23:23:49 +0600

    version 1.0.3

--- src/main/java/com/autobots/java/Version.java
+++ src/main/java/com/autobots/java/Version.java
@@ -5,5 +5,6 @@ public class Version {
         System.out.println("Version 1.0.0");
         System.out.println("Version 1.0.1");
         System.out.println("Version 1.0.2");
+        System.out.println("Version 1.0.3------>Bug");
     }
 }
